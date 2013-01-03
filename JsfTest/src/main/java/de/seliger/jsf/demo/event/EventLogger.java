acdff09d8dd525996181e57bfbc0335bc7db2c11
Jürgen Seliger <j.seliger@gmx.de>
2013-01-03 23:36:22 +0100

    EventLogger implementiert

--- JsfTest/src/main/java/de/seliger/jsf/demo/event/EventLogger.java
+++ JsfTest/src/main/java/de/seliger/jsf/demo/event/EventLogger.java
@@ -1,9 +1,18 @@
 package de.seliger.jsf.demo.event;
 
+import javax.enterprise.event.Observes;
+
+import org.apache.log4j.Logger;
 
 public class EventLogger {
 
-    // public static void logEvent(@Observes Event<?> event, Logger logger) {
-    // logger.debug("Event erhalten von: " + event.getClass().getName());
-    // }
+    private final static Logger LOGGER = Logger.getLogger(EventLogger.class);
+
+    public void logEvent(@Observes Object event) {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Event erhalten von: " + event.getClass().getName());
+        } else {
+            LOGGER.warn("Event erhalten von: " + event.getClass().getName());
+        }
+    }
 }
