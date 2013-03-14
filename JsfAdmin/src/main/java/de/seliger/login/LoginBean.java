a8a146c95421e918eb1eae6dabee91c2603f95a8
Jürgen Seliger <juergen.seliger@idos.de>
2013-03-14 15:53:29 +0100

    login hinzugefügt

---
+++ JsfAdmin/src/main/java/de/seliger/login/LoginBean.java
@@ -0,0 +1,24 @@
+package de.seliger.login;
+
+public class LoginBean {
+
+    private String user;
+    private String password;
+
+    protected String getUser() {
+        return user;
+    }
+
+    protected void setUser( String user ) {
+        this.user = user;
+    }
+
+    protected String getPassword() {
+        return password;
+    }
+
+    protected void setPassword( String password ) {
+        this.password = password;
+    }
+
+}
