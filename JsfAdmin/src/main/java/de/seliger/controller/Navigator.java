d073a89c2eb22993f3ca414e61252e78cfd798be
Jürgen Seliger <juergen.seliger@idos.de>
2013-03-14 15:29:27 +0100

    projekt initial hinzugefügt

---
+++ JsfAdmin/src/main/java/de/seliger/controller/Navigator.java
@@ -0,0 +1,12 @@
+package de.seliger.controller;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Navigator {
+
+    public String openMegamenu() {
+        return "openMegaMenu";
+    }
+
+}
